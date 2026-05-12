ae38f916286e741b6fa354a7a32887f7a6301fd8
jsjuhsju <jsjuhsju240@gmail.com>
2026-05-12 16:07:00 -0500

    Update illegal_system from Acode

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
 
+author 'IA Developer'
+description 'Sistema de Economía Ilegal - Propiedades con maquinaria, alcohol y drogas'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'config.lua',
+    'server/sv_utils.lua',
+    'server/main.lua'
+}
+
+dependencies {
+    'es_extended',
+    'ox_lib',
+    'ox_inventory'
+}
+
+lua54 'yes'
